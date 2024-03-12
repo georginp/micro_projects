2dfb3dc224197fb99261315efd359dfb633fb93f
Georgi Neshev <georgi.neshev@indeavr.com>
2024-03-12 13:39:26 +0200

    Added todo_list to micro_projects folder

---
+++ micro_projects/todo_list.py
@@ -0,0 +1,66 @@
+tasks = []
+
+
+def main_menu():
+    print()
+    print("===== To-Do List Menu =====")
+    print("1. Add a new task")
+    print("2. Mark a task as completed")
+    print("3. View all tasks")
+    print("4. Quit")
+    print()
+
+
+def add_task():
+    print()
+    task = input("Enter a task description:")
+    tasks.append({"task_description": task, "completed_status": False})
+    print(f"Task '{task}' added successfully!")
+    print()
+
+
+def mark_task_completed():
+    if tasks:
+        view_all_tasks()
+        task_index = int(input("Enter the index of the task to mark as completed: "))
+        if 0 <= task_index < len(tasks):
+            tasks[task_index]["completed_status"] = True
+            print(f"Task '{tasks[task_index]['task_description']}' marked as completed!")
+        else:
+            print("Please enter a valid index that can be found in the list!")
+        print()
+    else:
+        print("Please first add tasks to the list!")
+
+
+def view_all_tasks():
+    if tasks:
+        print("===== Tasks =====")
+        for index, task in enumerate(tasks):
+            status = "[X]" if task["completed_status"] else "[]"
+            print(f"{index}. {status} {task['task_description']}")
+    else:
+        print("Please first add tasks to the list!")
+
+
+def main():
+    while True:
+        main_menu()
+
+        choice = input(("Enter your choice (1-4): "))
+
+        if choice == "1":
+            add_task()
+        elif choice == "2":
+            mark_task_completed()
+        elif choice == "3":
+            view_all_tasks()
+        elif choice == "4":
+            print("Exiting the program. Goodbye!")
+            break
+        else:
+            print("Not a valid choice. Please select a valid option (1-4)")
+
+
+if __name__ == "__main__":
+    main()
